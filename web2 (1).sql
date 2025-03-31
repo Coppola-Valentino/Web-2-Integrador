0938a3be15c8c5a5ad8f47740b5529ebceffc6e9
Coppola-Valentino <espagueti6666@gmail.com>
2025-03-31 19:18:33 -0300

    Add files via upload

---
+++ web2 (1).sql
@@ -0,0 +1,216 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-04-2025 a las 00:17:42
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `web2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `camas`
+--
+
+CREATE TABLE `camas` (
+  `IDCama` int(10) NOT NULL,
+  `EnUso` tinyint(1) NOT NULL,
+  `Higenizada` tinyint(1) NOT NULL,
+  `Genero` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `citas`
+--
+
+CREATE TABLE `citas` (
+  `IDCita` int(100) NOT NULL,
+  `Tipo` varchar(255) NOT NULL,
+  `Medico` int(100) DEFAULT NULL,
+  `Fecha` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `habitacion`
+--
+
+CREATE TABLE `habitacion` (
+  `Camas` int(1) NOT NULL,
+  `Ala` tinytext NOT NULL,
+  `NumeroHab` int(100) NOT NULL,
+  `Genero` tinytext NOT NULL,
+  `Desinfeccion` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historial`
+--
+
+CREATE TABLE `historial` (
+  `IDHist` int(100) NOT NULL,
+  `Alergias` varchar(255) NOT NULL,
+  `Detalles` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medicos`
+--
+
+CREATE TABLE `medicos` (
+  `IDMed` int(100) NOT NULL,
+  `Nombre` varchar(255) DEFAULT NULL,
+  `DNI` int(100) DEFAULT NULL,
+  `Paciente` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paciente`
+--
+
+CREATE TABLE `paciente` (
+  `IDPaciente` int(11) NOT NULL,
+  `FullName` varchar(100) NOT NULL,
+  `Edad` int(3) NOT NULL,
+  `DNI` int(7) NOT NULL,
+  `SeguroActivo` varchar(100) NOT NULL,
+  `Genero` varchar(1) NOT NULL,
+  `Direccion` varchar(100) NOT NULL,
+  `Cama` int(11) NOT NULL,
+  `HistorialMed` int(11) NOT NULL,
+  `Cita` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `plandecuidado`
+--
+
+CREATE TABLE `plandecuidado` (
+  `IDPlan` int(100) NOT NULL,
+  `Tipo` varchar(255) DEFAULT NULL,
+  `Paciente` int(100) NOT NULL,
+  `PreparadoPor` int(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `camas`
+--
+ALTER TABLE `camas`
+  ADD PRIMARY KEY (`IDCama`);
+
+--
+-- Indices de la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD PRIMARY KEY (`IDCita`);
+
+--
+-- Indices de la tabla `habitacion`
+--
+ALTER TABLE `habitacion`
+  ADD PRIMARY KEY (`NumeroHab`);
+
+--
+-- Indices de la tabla `historial`
+--
+ALTER TABLE `historial`
+  ADD PRIMARY KEY (`IDHist`);
+
+--
+-- Indices de la tabla `medicos`
+--
+ALTER TABLE `medicos`
+  ADD PRIMARY KEY (`IDMed`);
+
+--
+-- Indices de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  ADD PRIMARY KEY (`IDPaciente`);
+
+--
+-- Indices de la tabla `plandecuidado`
+--
+ALTER TABLE `plandecuidado`
+  ADD PRIMARY KEY (`IDPlan`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `camas`
+--
+ALTER TABLE `camas`
+  MODIFY `IDCama` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `citas`
+--
+ALTER TABLE `citas`
+  MODIFY `IDCita` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `habitacion`
+--
+ALTER TABLE `habitacion`
+  MODIFY `NumeroHab` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `historial`
+--
+ALTER TABLE `historial`
+  MODIFY `IDHist` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `medicos`
+--
+ALTER TABLE `medicos`
+  MODIFY `IDMed` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  MODIFY `IDPaciente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `plandecuidado`
+--
+ALTER TABLE `plandecuidado`
+  MODIFY `IDPlan` int(100) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
