9cd4a437ebb6e7e5703d042e0aa00abcd8277267
Coppola-Valentino <espagueti6666@gmail.com>
2025-06-09 13:22:57 -0300

    vercel4

--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 const app = express();
-
+//index.js no esta en uso
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
